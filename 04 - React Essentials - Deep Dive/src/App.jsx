72edb0e8d8ff175e1b94bb7575f4bb54587f7f71
ahmad.aghazadeh <Ahmad.aghazadeh.a@gmail.com>
2024-12-29 12:08:51 +0330

    Add Deep Dive project

---
+++ 04 - React Essentials - Deep Dive/src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+   return (
+    <main>
+      <div id="game-container">
+        PLAYERS
+
+        GAME BOARD
+      </div>
+
+      LOGS
+    </main>
+  )
+}
+
+export default App
