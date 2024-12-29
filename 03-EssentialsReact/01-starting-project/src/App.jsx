1322e21238311bc9a2236964c8edbda97b0fb620
ahmad.aghazadeh <Ahmad.aghazadeh.a@gmail.com>
2024-12-29 09:23:02 +0330

    Add is render conditionally

--- 03-EssentialsReact/01-starting-project/src/App.jsx
+++ 03-EssentialsReact/01-starting-project/src/App.jsx
@@ -4,12 +4,14 @@ import { CORE_CONCEPTS } from './data';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
+import {EXAMPLES} from './data.js'
 
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+  const [selectedTopic, setSelectedTopic] = useState();
   function handleSelect(selectedButton){
     setSelectedTopic(selectedButton);
+    console.log(selectedButton)
   }
   return (
     <div> 
@@ -30,12 +32,28 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={()=>handleSelect("Components")}>Components</TabButton>
-            <TabButton onSelect={()=>handleSelect("JSX")}>JSX</TabButton>
-            <TabButton onSelect={()=>handleSelect("Props")}>Props</TabButton>
-            <TabButton onSelect={()=>handleSelect("State")}>State</TabButton>
+            <TabButton onSelect={()=>handleSelect("components")}>Components</TabButton>
+            <TabButton onSelect={()=>handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={()=>handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={()=>handleSelect("state")}>State</TabButton>
           </menu>
-          {selectedTopic}
+          {/* {!selectedTopic ? ( <p>Please select a topic. </p>) :(
+              <div id="tab-contnet">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+          </div>)
+          }  */}
+          {!selectedTopic && (<p>Please select a topic. </p>)}
+          {selectedTopic && (<div id="tab-contnet">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+          </div>)}
         </section>
       </main>
     </div>
