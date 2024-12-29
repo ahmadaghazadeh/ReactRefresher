7671b4d6279bebfc89a8cc299e19801699f4157f
ahmad.aghazadeh <Ahmad.aghazadeh.a@gmail.com>
2024-12-29 11:54:20 +0330

    add react wrapper

--- 03-EssentialsReact/01-starting-project/src/components/TabButton.jsx
+++ 03-EssentialsReact/01-starting-project/src/components/TabButton.jsx
@@ -1,7 +1,9 @@
-export default function TabButton({children,onSelect}){
-    return <li>
-        <button onClick={onSelect}>
-            {children}
-        </button>
+export default function TabButton({ children, isSelected, ...props }) {
+  return (
+    <li>
+      <button className={isSelected ? "active" : undefined} {...props}>
+        {children}
+      </button>
     </li>
+  );
 }
