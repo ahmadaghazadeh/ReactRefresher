9a56f5f2325a8df6716616d1d76813a587bba6a6
ahmadaghazadeh <ahmad.aghazadeh.a@gmail.com>
2025-02-14 18:19:09 +0330

    Add useEffect

---
+++ 11- Handling Side Effects Working with the useEffect Hook/src/components/Modal.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from 'react';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { createPortal } from 'react-dom';
+
+function Modal({ children ,open}) {
+  const dialog = useRef();
+
+  useEffect( () => {
+    if(open){
+      dialog.current.showModal();
+    }else{
+      dialog.current.close();
+    }
+  },[open]);
+
+  return createPortal(
+    <dialog className="modal" ref={dialog}>
+      {children}
+    </dialog>,
+    document.getElementById('modal')
+  );
+};
+
+export default Modal;
